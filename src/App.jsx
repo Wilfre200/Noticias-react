d10e365613d5d573ea608c02c35bb3ceca551788
Wilfredo <wilfredogarciadeleon@gmail.com>
2026-05-10 21:30:07 -0400

    Cambiando a GNews API

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,63 @@
-import { useEffect, useState } from 'react';
-
-import Home from './pages/Home';
-
-import './styles/app.css';
+import { useEffect, useState } from "react";
 
 function App() {
-
-  const [darkMode, setDarkMode] = useState(false);
+  const [news, setNews] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    const API_KEY = import.meta.env.VITE_GNEWS_API_KEY;
 
-    if (darkMode) {
-      document.body.style.backgroundColor = '#121212';
-    } else {
-      document.body.style.backgroundColor = '#f5f5f5';
-    }
+    fetch(
+      `https://gnews.io/api/v4/top-headlines?lang=es&country=do&max=10&apikey=${API_KEY}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setNews(data.articles);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        setLoading(false);
+      });
+  }, []);
 
-  }, [darkMode]);
-
-  const toggleDarkMode = () => {
-    setDarkMode(!darkMode);
-  };
+  if (loading) {
+    return <h1>Cargando noticias...</h1>;
+  }
 
   return (
+    <div style={{ padding: "20px" }}>
+      <h1>Noticias de República Dominicana</h1>
 
-    <div className={darkMode ? 'dark' : 'light'}>
+      {news.map((article, index) => (
+        <div
+          key={index}
+          style={{
+            border: "1px solid #ccc",
+            marginBottom: "20px",
+            padding: "15px",
+            borderRadius: "10px",
+          }}
+        >
+          <img
+            src={article.image}
+            alt={article.title}
+            style={{
+              width: "100%",
+              maxWidth: "500px",
+              borderRadius: "10px",
+            }}
+          />
 
-      <button
-        className="dark-mode-btn"
-        onClick={toggleDarkMode}
-      >
-        {darkMode ? '☀️ Light Mode' : '🌙 Dark Mode'}
-      </button>
+          <h2>{article.title}</h2>
 
-      <Home darkMode={darkMode} />
+          <p>{article.description}</p>
 
+          <a href={article.url} target="_blank">
+            Leer noticia completa
+          </a>
+        </div>
+      ))}
     </div>
   );
 }
